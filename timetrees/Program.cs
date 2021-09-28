b023cbacc7b14835f171621e9d0255846c79bac6
Pavel <thisonlystudy@gmail.com>
2021-09-28 14:33:30 +0500

    Ну почти V1.0 (Только разница месяцев не совсем корректная)

--- timetrees/Program.cs
+++ timetrees/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.IO;
 
 namespace timetrees
 {
@@ -6,7 +7,47 @@ namespace timetrees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string timeLineFile = "..\\..\\..\\..\\timeline.csv";
+            string[] lines = File.ReadAllLines(timeLineFile);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            string[][] timeLineData = ReadData(timeLineFile);
+            Console.WriteLine(FindMinAndMaxDate(timeLineData));
+            Console.WriteLine(DeltaDate(timeLineData));
+        }
+
+        static string[][] ReadData(string path)
+        {
+            string[] data = File.ReadAllLines(path);
+            string[][] splitData = new string[data.Length][];
+            for (int i=0; i<data.Length; i++)
+            {
+                var line = data[i];
+                string[] parts = line.Split(";");
+                splitData[i] = parts;
+            }
+            return splitData;
+        }     
+
+        static (DateTime, DateTime) FindMinAndMaxDate(string[][] timeline)
+        {
+            DateTime minDate = DateTime.MaxValue;
+            DateTime maxDate = DateTime.MinValue;
+            foreach (var line in timeline)
+            {
+                DateTime date = DateTime.Parse(line[0]);
+                if (date < minDate) minDate = date;
+                if (date > maxDate) maxDate = date;
+            }
+            return (maxDate, minDate);
+        }
+        
+        static (int, int, int) DeltaDate(string[][] timeline)
+        {
+            (DateTime  maxDate, DateTime minDate) = FindMinAndMaxDate(timeline);            
+            return (maxDate.Year - minDate.Year, maxDate.Month - minDate.Month, maxDate.Day - minDate.Day);
         }
     }
 }
