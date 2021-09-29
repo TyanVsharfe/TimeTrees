6a32ac08ef978e8e415b2e118f96982b971b889b
Pavel <thisonlystudy@gmail.com>
2021-09-29 21:07:13 +0500

    Что-то сломалось (почти V1.0)

--- timetrees/Program.cs
+++ timetrees/Program.cs
@@ -8,28 +8,42 @@ namespace timetrees
         static void Main(string[] args)
         {
             string timeLineFile = "..\\..\\..\\..\\timeline.csv";
+            string peopleFile = "..\\..\\..\\..\\people.csv";
+
             string[] lines = File.ReadAllLines(timeLineFile);
             foreach (var line in lines)
             {
                 Console.WriteLine(line);
             }
+            Console.WriteLine("");
+
+            string[] liness = File.ReadAllLines(peopleFile);
+            foreach (var line in liness)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("");
+
             string[][] timeLineData = ReadData(timeLineFile);
+            string[][] peopleData = ReadData(peopleFile);
             Console.WriteLine(FindMinAndMaxDate(timeLineData));
             Console.WriteLine(DeltaDate(timeLineData));
+            (int years, int months, int days) = DeltaDate(timeLineData);
+            Console.WriteLine($"ћежду макс и мин датами прошло: {years} лет, {months} мес€цев и {days} дней");          
         }
 
         static string[][] ReadData(string path)
         {
             string[] data = File.ReadAllLines(path);
             string[][] splitData = new string[data.Length][];
-            for (int i=0; i<data.Length; i++)
+            for (int i = 0; i < data.Length; i++)
             {
-                var line = data[i];
-                string[] parts = line.Split(";");
+                var line = data[i]; // "1; »м€; 2000-06-06
+                string[] parts = line.Split(";"); //["1", "»м€ 1", "2000-06-06"]
                 splitData[i] = parts;
             }
             return splitData;
-        }     
+        }
 
         static (DateTime, DateTime) FindMinAndMaxDate(string[][] timeline)
         {
@@ -43,11 +57,16 @@ namespace timetrees
             }
             return (maxDate, minDate);
         }
-        
+
         static (int, int, int) DeltaDate(string[][] timeline)
         {
-            (DateTime  maxDate, DateTime minDate) = FindMinAndMaxDate(timeline);            
+            (DateTime maxDate, DateTime minDate) = FindMinAndMaxDate(timeline);
             return (maxDate.Year - minDate.Year, maxDate.Month - minDate.Month, maxDate.Day - minDate.Day);
+        }
+
+        static void GetLeapYear(string[][] timeline)
+        {
+
         }
     }
 }
