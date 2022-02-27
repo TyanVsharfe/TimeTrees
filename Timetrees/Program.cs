23da796dc99e3b5ac57a66545f090a366f9762bd
Pavel <thisonlystudy@gmail.com>
2022-02-27 19:33:10 +0500

    V6.11

--- Timetrees/Program.cs
+++ Timetrees/Program.cs
@@ -30,14 +30,14 @@ namespace timetrees
             Console.CursorVisible = false;
             List<MenuItem> menu = new List<MenuItem>
             {
-                new MenuItem {Id = DeltaId,         Text = "����� ������ ��� ����� ���������", IsSelected = true},
-                new MenuItem {Id = AddPersonId,     Text = "�������� ��������"},
-                new MenuItem {Id = AddEventId,      Text = "�������� �������" },
-                new MenuItem {Id = EditPeopleId,    Text = "��������������� ������ ��������"},
-                new MenuItem {Id = LeapYearId,      Text = "����� �����, ���������� � ���������� ���"},
-                new MenuItem {Id = WritePeopleId,   Text = "������� ���� ����� � ������"},
-                new MenuItem {Id = WriteEventId,    Text = "������� ��� ������� � ������"},
-                new MenuItem {Id = ExitId,          Text = "�����"}
+                new MenuItem {Id = DeltaId,         Text = "Найти дельту дат между событиями", IsSelected = true},
+                new MenuItem {Id = AddPersonId,     Text = "Добавить человека"},
+                new MenuItem {Id = AddEventId,      Text = "Добавить событие" },
+                new MenuItem {Id = EditPeopleId,    Text = "Отредактировать данные человека"},
+                new MenuItem {Id = LeapYearId,      Text = "Найти людей, родившихся в високосный год"},
+                new MenuItem {Id = WritePeopleId,   Text = "Вывести всех людей в списке"},
+                new MenuItem {Id = WriteEventId,    Text = "Вывести все события в списке"},
+                new MenuItem {Id = ExitId,          Text = "Выход"}
             };
             bool exit = false;
             do
@@ -52,7 +52,7 @@ namespace timetrees
                     Execute(selectedItem.Id);
                     FileWriter.WritePeopleToJson();
                     FileWriter.WriteTimelineToJson();
-                    Console.WriteLine("������ ����������? Y/N");
+                    Console.WriteLine("Хотите продолжить? Y/N");
                     if (!AnswerLogic.GetAnswer())
                     {
                         Exit.DoExit();
